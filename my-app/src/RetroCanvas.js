a649b8ccff31b448866f23c17ed2bedf61033d37
Simon Sprouse <simonhsprouse@gmail.com>
2024-09-26 18:39:42 -0500

    directional line functions

--- my-app/src/RetroCanvas.js
+++ my-app/src/RetroCanvas.js
@@ -1,11 +1,15 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 function RetroCanvas() { 
 
     const canvasRef = useRef(null);
 
+    const aPos = useRef({ x:100, y:100 });
 
-    function drawLine(a, b, lineWidth, spacing) {
+    // const [direction, setDirection] = useState("Right");
+
+
+    function drawLine(a, b, lineWidth, spacing, direction="left") {
         const canvas = canvasRef.current;
         const ctx = canvas.getContext('2d');
 
@@ -19,19 +23,72 @@ function RetroCanvas() {
             '#FFD37C'  // Yellow
         ];
 
-        colors.forEach((color, index) => { 
-            ctx.beginPath();
-            ctx.strokeStyle = color;
 
-            const y = a.y + index * (spacing / (colors.length - 1));
+        if (direction == "left") { 
+            colors.forEach((color, index) => { 
+                ctx.beginPath();
+                ctx.strokeStyle = color;
+
+                const y = a.y + index * (spacing / (colors.length - 1));
+    
+                ctx.moveTo(a.x, y);
+                ctx.lineTo(b.x, y);
+    
+                ctx.stroke();
+            });
+            aPos.current = {x: b.x, y: a.y};
+        }
+        else if (direction == "right") { 
+            colors.forEach((color, index) => { 
+                ctx.beginPath();
+                ctx.strokeStyle = color;
+
+                const y = a.y - index * (spacing / (colors.length - 1));
+
+                ctx.moveTo(a.x, y);
+                ctx.lineTo(b.x, y);
+
+                ctx.stroke();
+            });
+            aPos.current = {x:b.x , y: a.y};
+        }
+        else if (direction == "up") { 
+
+            colors.forEach((color, index) => { 
+                ctx.beginPath();
+                ctx.strokeStyle = color;
+
+                const x = a.x + index * (spacing / (colors.length - 1));
+
+                ctx.moveTo(x, a.y);
+                ctx.lineTo(x, b.y);
+
+                ctx.stroke();
+            });
 
 
-            ctx.moveTo(a.x, y);
-            ctx.lineTo(b.x, y);
+            aPos.current = {x:a.x , y: b.y};
+        }
+        else if (direction == "down") { 
+            aPos.current = {x:a.x , y: b.y};
+
+            colors.forEach((color, index) => { 
+                ctx.beginPath();
+                ctx.strokeStyle = color;
+
+                const x = a.x - index * (spacing / (colors.length - 1));
+
+                ctx.moveTo(x, a.y);
+                ctx.lineTo(x, b.y);
+
+                ctx.stroke();
+            });
+        }
 
 
-            ctx.stroke();
-        })
+        
+
+        
 
     };
 
@@ -87,12 +144,18 @@ function RetroCanvas() {
     }, []);
 
     function test() { 
-        const pointA = { x:100, y:100};
-        const pointB = { x:700, y:100};
-        const pointC = { x:800, y:100}
+        const pointA = { x:100, y:100 };
+        const pointB = { x:700, y:100 };
+        const pointC = { x:100, y:400 };
+        const pointD = { x:700, y:400 };
         drawLine(pointA, pointB, 10, 100);
         // drawLine(pointB, pointC, 10, 100);
-        drawArc(pointB, 30, 10, 100);
+        // drawArc(pointB, 30, 10, 100);
+
+
+        drawLine(pointC, pointD, 10, 100, "right");
+        drawLine(pointA, pointC, 10, 100, "up");
+        drawLine(pointD, pointB, 10, 100, "down");
         
     }
 
